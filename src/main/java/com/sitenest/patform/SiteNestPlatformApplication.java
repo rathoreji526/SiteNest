069f4eab608d0138bbad46f70026aa5ed10ad87f
Kamal Rathore <mr.rathoreji52@gmail.com>
2025-12-17 14:02:11 +0530

    added boiler plate spring project with some dependencies

---
+++ src/main/java/com/sitenest/patform/SiteNestPlatformApplication.java
@@ -0,0 +1,13 @@
+package com.sitenest.patform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SiteNestPlatformApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SiteNestPlatformApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sitenest/patform/SiteNestPlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sitenest.patform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SiteNestPlatformApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
